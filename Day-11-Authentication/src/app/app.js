e420de8fa2ba3f09bf4bc18a507284f3feb8f1e7
Pratham Kave <kavepratham@gmail.com>
2026-09-03 15:21:10 +0530

    Build /me for headers

--- Day-11-Authentication/src/app/app.js
+++ Day-11-Authentication/src/app/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 
+import userModel from "../models/user.model.js";
+
 const app = express();
 
 app.use(express.json());
@@ -9,14 +11,20 @@ app.get("/api", (req, res) => {
 	res.status(200).json({ message: "Welcome to the Authentication API" });
 });
 
-app.post("/api/auth/register", (req, res) => {
+app.post("/api/auth/register", async (req, res) => {
 	const { email, name, password } = req.body;
+
+	const user = await userModel.create({
+		email,
+		name,
+		password,
+	});
+
 	const token = jwt.sign(
 		{
-			email,
-			name,
+			id: user._id,
 		},
-		"11fff6bb93fce87d83934f3849aaa9a5b540ac69bf736cc20a248c82dc788165",
+		process.env.JWT_SECRET,
 	);
 
 	res.status(201).json({
@@ -25,10 +33,18 @@ app.post("/api/auth/register", (req, res) => {
 			user: {
 				email,
 				name,
+				id: user._id,
 			},
 			token,
 		},
 	});
+});
+
+app.get("/api/auth/me", async (req, res) => {
+	const authHeader = req.headers.authorization;
+	const data = jwt.decode(authHeader);
+	const user = await userModel.findById(data.id);
+	console.log(user);
 });
 
 export default app;
--- Day-11-Authentication/src/server.js
+++ Day-11-Authentication/src/server.js
@@ -1,7 +1,8 @@
 import app from "./app/app.js";
+import { connectDB } from "./config/db.js";
 
-const PORT = process.env.PORT || 3000;
+await connectDB();
 
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+app.listen(3000, () => {
+	console.log(`Server is running on port 3000`);
 });
