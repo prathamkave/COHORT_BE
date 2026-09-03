9ca5c085325ab3f9e0384ed4dadf49f3b5623ea6
Pratham Kave <kavepratham@gmail.com>
2026-09-03 16:09:12 +0530

    bcryptjs and Login route created successfully

--- Day-11-Authentication/src/app/app.js
+++ Day-11-Authentication/src/app/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-
+import bcrypt from "bcryptjs";
+import { authenticate } from "../middleware/auth.middleware.js";
 import userModel from "../models/user.model.js";
 
 const app = express();
@@ -17,7 +18,7 @@ app.post("/api/auth/register", async (req, res) => {
 	const user = await userModel.create({
 		email,
 		name,
-		password,
+		password: await bcrypt.hash(password, 10),
 	});
 
 	const token = jwt.sign(
@@ -40,11 +41,40 @@ app.post("/api/auth/register", async (req, res) => {
 	});
 });
 
-app.get("/api/auth/me", async (req, res) => {
-	const authHeader = req.headers.authorization;
-	const data = jwt.decode(authHeader);
-	const user = await userModel.findById(data.id);
-	console.log(user);
+app.get("/api/auth/me", authenticate, (req, res) => {
+	res.status(200).json({
+		data: {
+			user: req.user,
+		},
+	});
+});
+
+app.post("/api/auth/login", async (req, res) => {
+	const { email, password } = req.body;
+
+	const user = await userModel.findOne({ email });
+
+	const isValidPassword = await bcrypt.compare(password, user.password);
+
+	if (!isValidPassword) {
+		return res.status(401).json({
+			message: "Invalid credentials",
+		});
+	}
+
+	const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+	res.status(200).json({
+		message: "User logged in successfully",
+		data: {
+			user: {
+				email: user.email,
+				name: user.name,
+				id: user._id,
+			},
+			token,
+		},
+	});
 });
 
 export default app;
