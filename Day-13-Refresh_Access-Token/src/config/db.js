9d08b7a92377937a6f2115727901360756d4808d
Pratham Kave <kavepratham@gmail.com>
2026-09-09 22:45:34 +0530

    Folder Updated & Package added

--- Day-13-Refresh_Access-Token/src/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import config from "./config.js";
-
-async function connectDB() {
-	await mongoose.connect(config.MONGO_URI);
-	console.log("Connected to the database");
-}
-
-export default connectDB;
--- Day-13-Refresh_Access-Token/src/server.js
+++
@@ -1,8 +0,0 @@
-import app from "./app/app.js";
-import connectDB from "./config/db.js";
-
-await connectDB();
-
-app.listen(3000, () => {
-	console.log("Server is running on port 3000");
-});
